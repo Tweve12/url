f31bd6f016b0588506dee5f252ae4b71c263681e
Tweve12 <vmakarovskaa3@gmail.com>
2024-12-16 18:38:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let input = document.querySelector(".search__input");
+let button = document.querySelector(".search__button");
+let select = document.querySelector(".search__select");
+let params;
+
+
+function sumbitRequest(){
+    let getInputVal = input.value;
+    let getSelectVal = select.value;
+    if (getSelectVal == "go"){
+        
+    }
+}
+
+button.addEventListener("click", sumbitRequest);
